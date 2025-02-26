29597a4af620bf210811dcb32e7356dca194a39e
Carlos Augusto <caarlos.frei@gmail.com>
2025-02-26 09:57:26 -0300

    refactor(agenda): refatorar código para organizar a exibição da agenda

--- lib/textContent.ts
+++ lib/textContent.ts
@@ -10,7 +10,8 @@ export const text_content_band = {
 
 export const text_content_agenda = {
   agendaTitle: `Agenda de Shows`,
-  agendaDescription: `Willi Willie Bar e Arqueria é um dos principais pontos de encontro para os fãs de rock em São Paulo. Com uma estrutura completa e um ambiente descontraído, o local é perfeito para quem quer curtir um bom show e se divertir com os amigos. A Velvet Roses Tribute Cover se apresenta no dia 02 de Abril, trazendo o melhor do rock clássico e do hard rock para o público. Não perca essa oportunidade de reviver os maiores sucessos de Slash e Myles Kennedy em uma noite inesquecível!`
+  agendaDescription: `Willi Willie Bar e Arqueria é um dos principais pontos de encontro para os fãs de rock em São Paulo. Com uma estrutura completa e um ambiente descontraído, o local é perfeito para quem quer curtir um bom show e se divertir com os amigos. A Velvet Roses Tribute Cover se apresenta no dia 02 de Abril, trazendo o melhor do rock clássico e do hard rock para o público. Não perca essa oportunidade de reviver os maiores sucessos de Slash e Myles Kennedy em uma noite inesquecível!`,
+  agendaImage: '/images/logowillidatabranco.png'
 };
 
 export const text_content_contact = {
--- pages/agenda/Index.tsx
+++ pages/agenda/Index.tsx
@@ -1,27 +1,78 @@
 import BlurFade from "@/components/ui/Blur-Fade";
 import { FadeText } from "@/components/ui/fade-text";
-import './Index.css';
+import PulsatingButton from "@/components/ui/pulsating-button";
+import "./Index.css";
+import Image from 'next/image';
 import { text_content_agenda } from "@/lib/textContent";
 
 export default function AgendaPage() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-black text-white px-4 space-y-8">
       <BlurFade delay={0.25} inView>
-        <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none">
+        <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none mt-60">
           AGENDA
         </h2>
       </BlurFade>
-      <FadeText
-        className="text-4xl font-bold text-white"
-        direction="up"
-        framerProps={{
-          show: { transition: { delay: 0.8 } },
-        }}
-        text="02 de Abril - Willi Willie Bar e Arqueria"
-      />
-      <p className="text-lg text-gray-400 max-w-xl">
-        {text_content_agenda.agendaDescription}
-      </p>
+
+      {/* Container centralizado da Agenda */}
+      <div className="flex justify-center w-full">
+        <div className="grid grid-cols-2 gap-12 w-full max-w-3xl text-lg items-center text-center">
+          {/* Coluna 1 - Data e Local */}
+          <div className="font-bold text-white">
+            <FadeText
+              className="text-2xl font-bold text-white"
+              direction="up"
+              framerProps={{ show: { transition: { delay: 0.8 } } }}
+              text="02 de Abril - 21h"
+            />
+
+            <FadeText
+              className="text-1xl font-bold text-white"
+              direction="up"
+              framerProps={{ show: { transition: { delay: 0.8 } } }}
+              text="Willi Willie Bar e Arqueria"
+            />
+
+            <FadeText
+              className="text-xl font-bold text-black flex justify-center mt-5"
+              direction="up"
+              framerProps={{ show: { transition: { delay: 0.8 } } }}
+              text={
+                <div className="flex justify-center">
+                  <PulsatingButton>
+                    <a
+                      href="https://forms.gle/zNzASK4vwZ8XpL3j6"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="text-base block px-3 py-1 rounded-lg text-black hover:bg-white transition-all"
+                    >
+                      Lista VIP
+                    </a>
+                  </PulsatingButton>
+                </div>
+              }
+            />
+
+          </div>
+
+          {/* Coluna 2 - Informações sobre o local e show */}
+          <div className="flex flex-col items-center space-y-4">
+            <Image
+              src={text_content_agenda.agendaImage}
+              alt="img"
+              width={100}
+              height={100}
+              className="rounded-lg"
+            />
+            {/* Texto */}
+            <div className="text-gray-300 text-base text-justify">
+              {text_content_agenda.agendaDescription}
+            </div>
+          </div>
+
+        </div>
+      </div>
+
       <div className="agenda-social flex space-x-4">
         <a
           href="https://www.instagram.com/slashcover.oficial/"
@@ -49,6 +100,7 @@ export default function AgendaPage() {
         </a>
       </div>
 
+      {/* Vídeo de fundo */}
       <div className="relative w-full h-screen">
         <video
           className="absolute top-0 left-0 w-full h-full opacity-50"
@@ -56,22 +108,12 @@ export default function AgendaPage() {
           muted
           loop
           playsInline
-          controls={false} // Desativa os controles
+          controls={false}
         >
           <source src="/video/video.mp4" type="video/mp4" />
           Seu navegador não suporta o elemento de vídeo.
         </video>
-        {/* <iframe
-          className="absolute top-0 left-0 w-full h-full opacity-50"
-          src="https://www.youtube.com/embed/s7TLDpOQmxY?autoplay=1&mute=1&controls=0&disablekb=1&fs=0&modestbranding=1&autohide=1&rel=0"
-          title="YouTube video player"
-          frameBorder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
-        ></iframe> */}
       </div>
-
-
     </div>
   );
 }
