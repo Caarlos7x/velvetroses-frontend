99e740212ee258c3578c4bdf870875edefc5b65f
Carlos Augusto <caarlos.frei@gmail.com>
2025-02-26 11:34:14 -0300

    feat(agenda): correção txt botão lista.

--- pages/agenda/Index.tsx
+++ pages/agenda/Index.tsx
@@ -46,7 +46,7 @@ export default function AgendaPage() {
                       rel="noopener noreferrer"
                       className="text-base block px-3 py-1 rounded-lg text-black hover:bg-white transition-all"
                     >
-                      Nome na Lista
+                      Nome na Lista 30%OFF
                     </a>
                   </PulsatingButton>
                 </div>
