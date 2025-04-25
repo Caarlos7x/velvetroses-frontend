d75cbbac4d2e9a4942436c30e6a203d67a921707
Carlos Augusto <caarlos.frei@gmail.com>
2025-04-25 13:33:15 -0300

    adição de show

--- components/tour/Tour.tsx
+++ components/tour/Tour.tsx
@@ -22,22 +22,14 @@ const shows: Show[] = [
     deadline: "2025-04-02T22:00:00",
     discount: 33,
   },
-  // {
-  //   date: "26 de Abril, Sábado, 2025",
-  //   event: "Hot Pub SA",
-  //   venue: "Av. Arthur de Queiroz, 75",
-  //   location: "Santo André, SP",
-  //   deadline: "2025-04-26T22:00:00",
-  //   discount: 0,
-  // },
-  // {
-  //   date: "10 de Maio, Sábado, 2025",
-  //   event: "Blackbird Rock Beer",
-  //   venue: "Rua Carijós, 131",
-  //   location: "Santo André, SP",
-  //   deadline: "2025-05-10T22:00:00",
-  //   discount: 0,
-  // },
+  {
+    date: "18 de Maio, Sábado, 2025",
+    event: "Blackbird Rock Beer",
+    venue: "Rua Carijós, 131",
+    location: "Santo André, SP",
+    deadline: "2025-05-18T22:00:00",
+    discount: 0,
+  },
 ];
 
 export default function WorldTourAgenda() {
